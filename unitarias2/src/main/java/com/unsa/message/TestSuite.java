ce6b60fc1605f83a664d3c8d561d0a26ce10540f
ahincho <ahincho@unsa.edu.pe>
2023-06-09 06:18:41 -0500

    Revision e identacion

--- unitarias2/src/main/java/com/unsa/message/TestSuite.java
+++ unitarias2/src/main/java/com/unsa/message/TestSuite.java
@@ -5,10 +5,12 @@ import org.junit.runners.Suite;
 
 public class TestSuite {
     @RunWith(Suite.class)
-    @Suite.SuiteClasses({
-        TestJUnit1.class,
-        TestJUnit2.class
-    })
+    @Suite.SuiteClasses(
+        {
+            TestJUnit1.class,
+            TestJUnit2.class
+        }
+    )
     public class JUnitTestSuite {
     }
 }
